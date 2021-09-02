a69e3b77cc6edf4a760461be37db1e3ac78c77ad
Sam Sepiel <pojskicsafet@gmail.com>
2021-09-02 14:05:22 +0200

    Page now displays data from the mock database

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Header from "./components/Header";
 import Tasks from "./components/Tasks";
 import AddTask from "./components/AddTask";
 
 function App() {
   const [showAddTask, setShowAddTask] = useState(false);
-
   const [tasks, setTasks] = useState([]);
+
+  useEffect(() => {
+    const getTasks = async () => {
+      const tasksFromServer = await fetchTasks();
+      setTasks(tasksFromServer)
+    }
+    getTasks();
+  }, [])
+
+  //Fetch tasks
+  const fetchTasks = async () => {
+    const res = await fetch('http://localhost:5000/tasks')
+    const data = await res.json();
+    return data;
+  }
 
   //Add Task
   const addTask = (task) => {
